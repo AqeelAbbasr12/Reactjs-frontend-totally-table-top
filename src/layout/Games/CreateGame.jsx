d4e336dfa8893aff2175dee9f4d597c0fc4ccec5
Aqeel Abbas <aqeelabbasr12@gmail.com>
2024-07-24 13:08:55 +0100

    fixes

--- src/layout/Games/CreateGame.jsx
+++ src/layout/Games/CreateGame.jsx
@@ -1,20 +1,21 @@
-import React, {useState} from 'react'
-import Navbar from '../../components/Navbar'
-import { FaList } from 'react-icons/fa'
-import { useNavigate } from 'react-router-dom'
-import { BsFillCaretDownFill, BsFillCaretUpFill } from 'react-icons/bs'
-import Button from '../../components/Button'
-import Input from '../../components/Input'
+import React, { useState } from "react";
+import Navbar from "../../components/Navbar";
+import { FaList } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import IconCaretSvg from "../../assets/icon-caret-down.svg";
 
 const CreateGame = () => {
-    const nav = useNavigate()
-    const CONDITIONS = [
-    'Brand new',
-    'Excellent',
-    'Good',
-    'Fair',
-    'Below Average'
-    ];
+  const nav = useNavigate();
+  const CONDITIONS = [
+    "Brand new",
+    "Excellent",
+    "Good",
+    "Fair",
+    "Below Average",
+  ];
 
   const [isOpen, setIsOpen] = useState(false);
 
@@ -22,95 +23,149 @@ const CreateGame = () => {
     setIsOpen(!isOpen);
   };
 
-    return (
-        <div className='flex flex-col w-[100vw] h-[100vh] overflow-y-auto'>
-            <Navbar type={"verified"} />
+  return (
+    <div className="flex flex-col w-[100vw] h-[100vh] overflow-y-auto">
+      <Navbar type={"verified"} />
 
-            <div className='bg-black md:px-[2rem] px-[1rem] flex items-center gap-x-4 py-3 '>
-                <span className='text-white'>Account</span>
-                <BsFillCaretDownFill className=' text-lightOrange -rotate-90' />
-                <span className='text-white'>Your conventions</span>
-                <BsFillCaretDownFill className=' text-lightOrange -rotate-90' />
-                <span className='text-white'>Uk Games Expo 2024</span>
+      <div className="bg-black md:px-[2rem] px-[1rem] flex items-center gap-x-4 py-3 ">
+        <span className="text-white">Account</span>
+        <BsFillCaretDownFill className=" text-lightOrange -rotate-90" />
+        <span className="text-white">Your conventions</span>
+        <BsFillCaretDownFill className=" text-lightOrange -rotate-90" />
+        <span className="text-white">Uk Games Expo 2024</span>
+      </div>
+
+      <div className="md:px-[2rem] px-[1rem] bg-darkBlue h-[86vh] w-[100vw] py-3 flex justify-center items-center overflow-y-auto">
+        <div className="sm:w-[50%] w-[100%] bg-[#0d2539]  px-3 py-5 rounded-md mt-6">
+          <div className="flex justify-center items-center">
+            <div className="w-[3rem] h-[3rem] rounded-full bg-lightOrange flex justify-center items-center">
+              UKGE
             </div>
+            <div className="w-[3rem] h-[3rem] rounded-full bg-lightOrange flex justify-center items-center">
+              <FaList className="text-white" />
+            </div>
+          </div>
 
+          <div className="pb-4">
+            <h1 className="text-3xl mt-3 text-center text-white font-semibold">
+              Add new game
+            </h1>
+          </div>
 
-            <div className='md:px-[2rem] px-[1rem] bg-darkBlue h-[86vh] w-[100vw] py-3 flex justify-center items-center overflow-y-auto'>
+          <div className="pb-2">
+            <Input
+              name={"name"}
+              placeholder={"Game Name"}
+              type={"text"}
+              onChangeFunc={(e) => console.log(e)}
+              className={`w-[100%] h-[3rem] rounded-md text-white px-4 mt-2 outline-none bg-darkBlue`}
+            />
+          </div>
 
-                <div className='sm:w-[50%] w-[100%] bg-[#0d2539]  px-3 py-5 rounded-md mt-6'>
-                    <div className='flex justify-center items-center'>
-                        <div className='w-[3rem] h-[3rem] rounded-full bg-lightOrange flex justify-center items-center'>UKGE</div>
-                        <div className='w-[3rem] h-[3rem] rounded-full bg-lightOrange flex justify-center items-center'><FaList className='text-white' /></div>
-                    </div>
-
-                    <div className='pb-4'>
-                        <h1 className='text-3xl mt-3 text-center text-white font-semibold'>Add new game</h1>
-                    </div>
-
-                    <div className='pb-2'>
-                        <Input name={"name"} placeholder={"Game Name"} type={"text"} onChangeFunc={(e) => console.log(e)} className={`w-[100%] h-[3rem] rounded-md text-white px-4 mt-2 outline-none bg-darkBlue`} />
-                    </div>
-
-                    <div className='flex justify-center items-center md:flex-row flex-col mt-2 gap-x-4 pb-2'>
-                        <div className='flex justify-between px-2 items-center h-[3rem] w-[100%] cursor-pointer rounded-md text-white px-4 outline-none bg-darkBlue'>
-                            <div>
-                            <p className='text-white'>Select Price</p>
-                            </div>
-                            <div className='flex item-end gap-x-2'>
-                                <span className='text-white'>USD</span>
-                                <BsFillCaretDownFill className='text-white ' />
-                            </div>
-
-                            
-                        </div>
-
-                    
-                    <div
-                        className='flex justify-between px-2 items-center h-[3rem] w-[100%] cursor-pointer rounded-md text-white px-4 outline-none bg-darkBlue'
-                        onClick={handleToggle} >
-                        <p className='text-white'>Select Condition</p>
-                        {isOpen ? (
-                        <BsFillCaretUpFill className='text-white' />
-                        ) : (
-                        <BsFillCaretDownFill className='text-white' />
-                        )}
-
-                    {isOpen && (
-                        <ul className='mt-2 bg-darkBlue top-[415px] rounded-md text-white absolute z-10 origin-top-right' tabindex="-1">
-                        {CONDITIONS.map((condition, index) => (
-                            <li
-                            key={index}
-                            className='px-4 py-2 cursor-pointer hover:bg-gray-700'
-                            >
-                            {condition}
-                            </li>
-                        ))}
-                        </ul>
-                    )}
-                    </div>
-
-                    </div>
-
-                    <div className='pb-2'>
-                        <Input name={"name"} placeholder={"Category"} type={"text"} onChangeFunc={(e) => console.log(e)} className={`w-[100%] h-[3rem] rounded-md text-white px-4 mt-2 outline-none bg-darkBlue`} />
-                    </div>
-
-                    <div className='pb-2'>
-                        <Input name={"name"} placeholder={"Description"} type={"text"} onChangeFunc={(e) => console.log(e)} className={`w-[100%] h-[3rem] rounded-md text-white px-4 mt-2 outline-none bg-darkBlue`} />
-                    </div>
-
-                    <div className='pb-2'>
-                        <Input name={"name"} placeholder={"Upload Image"} type={"text"} onChangeFunc={(e) => console.log(e)} className={`w-[100%] h-[3rem] rounded-md text-white px-4 mt-2 outline-none bg-darkBlue`} />
-                    </div>
-
-                    <div className='flex justify-center items-center mt-4'>
-                        <Button onClickFunc={() => nav("/game/sale")} title={"List Game"} className={`w-[12rem] h-[3rem] rounded-md text-white bg-lightOrange`} />
-                    </div>
+          <div className="flex justify-center items-center md:flex-row flex-col mt-2 gap-x-4 pb-2">
+            <div className="flex justify-between relative rounded-md w-[100%]">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <span className="text-gray-500 text-white">$</span>
+              </div>
+              <input
+                type="text"
+                name="price"
+                id="price"
+                className="block w-full py-1.5 pl-7 pr-20 h-[3rem] w-[100%] cursor-pointer rounded-md text-white px-4 outline-none bg-darkBlue"
+                placeholder="price..."
+              />
+              <div className="absolute inset-y-0 right-0 flex items-center text-white">
+                <div className="relative">
+                  <select
+                    id="currency"
+                    name="currency"
+                    className="h-full appearance-none rounded-md py-0 pl-2 pr-8 text-white outline-none bg-darkBlue"
+                  >
+                    <option>USD</option>
+                    <option>CAD</option>
+                    <option>EUR</option>
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <img
+                      className="w-3 h-3 text-white"
+                      src={IconCaretSvg}
+                      alt=""
+                    />
+                  </div>
                 </div>
-
+              </div>
             </div>
-        </div>
-    )
-}
 
-export default CreateGame
+            <div
+              className="flex justify-between px-2 items-center h-[3rem] w-[100%] cursor-pointer rounded-md text-white px-4 outline-none bg-darkBlue"
+              onClick={handleToggle}
+            >
+              <p className="text-white">Select Condition</p>
+              {isOpen ? (
+                <BsFillCaretUpFill className="text-white" />
+              ) : (
+                <BsFillCaretDownFill className="text-white" />
+              )}
+
+              {isOpen && (
+                <ul
+                  className="mt-2 bg-darkBlue top-[415px] rounded-md text-white absolute z-10 origin-top-right"
+                  tabindex="-1"
+                >
+                  {CONDITIONS.map((condition, index) => (
+                    <li
+                      key={index}
+                      className="px-4 py-2 cursor-pointer hover:bg-gray-700"
+                    >
+                      {condition}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div>
+
+          <div className="pb-2">
+            <Input
+              name={"name"}
+              placeholder={"Category"}
+              type={"text"}
+              onChangeFunc={(e) => console.log(e)}
+              className={`w-[100%] h-[3rem] rounded-md text-white px-4 mt-2 outline-none bg-darkBlue`}
+            />
+          </div>
+
+          <div className="pb-2">
+            <Input
+              name={"name"}
+              placeholder={"Description"}
+              type={"text"}
+              onChangeFunc={(e) => console.log(e)}
+              className={`w-[100%] h-[3rem] rounded-md text-white px-4 mt-2 outline-none bg-darkBlue`}
+            />
+          </div>
+
+          <div className="pb-2">
+            <Input
+              name={"name"}
+              placeholder={"Upload Image"}
+              type={"text"}
+              onChangeFunc={(e) => console.log(e)}
+              className={`w-[100%] h-[3rem] rounded-md text-white px-4 mt-2 outline-none bg-darkBlue`}
+            />
+          </div>
+
+          <div className="flex justify-center items-center mt-4">
+            <Button
+              onClickFunc={() => nav("/game/sale")}
+              title={"List Game"}
+              className={`w-[12rem] h-[3rem] rounded-md text-white bg-lightOrange`}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateGame;
